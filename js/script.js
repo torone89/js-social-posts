04ed465042d5ce931a6b34098326a47c666815cc
torone89 <torone89>
2022-07-06 15:23:47 +0200

    Milestone 2 Fatta

--- js/script.js
+++ js/script.js
@@ -87,3 +87,44 @@ console.log(post);
 
 // / #Milestone 2
 // Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
+
+// Recupero il container dadl Dom dove sucessivamente stampare la mia card
+const container = document.getElementById('container');
+
+// const { idpost, autore, imgautore, data, contenuto, img, likes } = datiCard
+
+for (let i = 0; i < post.length; i++) {
+    const stampo = post[i];
+
+    container.innerHTML += `<div class="post">
+<div class="post__header">
+  <div class="post-meta">
+    <div class="post-meta__icon">
+      <img class="profile-pic" src="${stampo.imgautore}" alt="${stampo.autore}" />
+    </div>
+    <div class="post-meta__data">
+      <div class="post-meta__author">${stampo.autore}</div>
+      <div class="post-meta__time">${stampo.data}</div>
+    </div>
+  </div>
+</div>
+<div class="post__text">
+${stampo.contenuto}
+</div>
+<div class="post__image">
+  <img src="${stampo.img}" alt="Imaggine n ${stampo.id}" />
+</div>
+<div class="post__footer">
+  <div class="likes js-likes">
+    <div class="likes__cta">
+      <a class="like-button js-like-button" href="#" data-postid="${stampo.id}">
+        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+        <span class="like-button__label">Mi Piace</span>
+      </a>
+    </div>
+    <div class="likes__counter">Piace a<b id="like-counter-1" class="js-likes-counter">${stampo.likes}</b> persone</div>
+  </div>
+</div>
+</div>`
+}
